fcd6c30fdd64b5d75d2fb83e1c39062eacf11ce8
ckt9636 <ckt9636@gmail.com>
2018-04-05 14:41:34 +0900

    test example

---
+++ src/main/scala/com/ckjteam/Example_Join.scala
@@ -0,0 +1,30 @@
+package com.ckjteam
+
+import org.apache.spark.sql.SparkSession;
+
+object Example_Join {
+
+  val spark = SparkSession.builder().appName("...").
+    config("spark.master", "local").
+    getOrCreate()
+
+  var dataPath = "c:/spark/bin/data/"
+  var mainData = "kopo_channel_seasonality_ex.csv"
+  var subData = "kopo_product_mst.csv"
+
+  var mainDataDf = spark.read.format("csv").
+    option("header", "true").
+    load(dataPath + mainData)
+  var subDataDf = spark.read.format("csv").
+    option("header", "true").
+    load(dataPath + subData)
+
+  mainDataDf.createOrReplaceTempView("mainData")
+  subDataDf.createOrReplaceTempView("subData")
+
+
+
+
+
+
+}
---
+++ src/main/scala/com/ckjteam/Test.scala
@@ -0,0 +1,31 @@
+package com.ckjteam
+import org.apache.spark.sql.SparkSession
+
+object Example_DataLoading {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder().appName("hkProject").
+      config("spark.master", "local").
+      getOrCreate()
+
+
+
+    var staticUrl = "jdbc:oracle://192.168.110.111:1521/orcl"
+    var staticUser = "kopo"
+    var staticPw = "kopo"
+    var selloutDb = "kopo_channel_seasonality_ex"
+
+    //jdbc(java database connectivity)
+   // val selloutData = spark.read.format("jdbc").
+    //  options(Map("url" -> staticUrl, "dbtable" -> selloutDb, "user" -> staticUser, "password" -> staticPw)).load
+   // selloutData.createTempView("maindata")
+    //selloutData.show(1)
+   val selloutDataFromPg= spark.read.format("jdbc").
+     options(Map("url" -> staticUrl,"dbtable" -> selloutDb,"user" -> staticUser, "password" -> staticPw)).load
+
+    // 메모리 테이블 생성
+    selloutDataFromPg.createOrReplaceTempView("selloutTable")
+    selloutDataFromPg.show(1)
+
+
+  }
+  }
